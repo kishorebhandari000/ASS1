3cb41384c19f213a3a1d98d807c0276235a7db2c
Kishore Bhandari <kishorebhandari000@gmail.com>
2026-03-29 20:44:46 +1100

    updated

---
+++ JS/Conditions.js
@@ -0,0 +1,30 @@
+let students=[
+  {name:"kishore",age:24,registration:true},
+  {name:"pranish",age:25,registration:false},
+  {name:"sirisha",age:24,registration:true},
+    {name:"shramy",age:23,registration:false},
+];
+// console.log("all students: ",students);
+// students.forEach(u=>{
+//   if(u.registration){
+//     console.log(u.name+" is registered");
+//   }
+//   else{
+// console.log(u.name+ " is not registered");
+//   }
+// });
+
+// let registered=students.filter(i=>i.registration);
+// let notRegistered=students.filter(i=>!i.registration);
+// console.log("Registered students: ",registered );
+// console.log("Unregistered students: ",notRegistered );
+
+console.log("Registered students are: ",students
+  .filter(i=>i.registration)
+  .map(i=>i.name)
+);
+
+console.log("unregistered students are: ", students
+  .filter(i=>!i.registration)
+.map(i=>i.name)
+);
