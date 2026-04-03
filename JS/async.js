db15f8e024b0651fa886664af33fe71bd544c20d
Kishore Bhandari <kishorebhandari000@gmail.com>
2026-04-03 11:04:12 +1100

    Asyn practice

--- JS/async.js
+++ JS/async.js
@@ -21,18 +21,18 @@
 // // localStorage.setItem("user", JSON.stringify(user));
 
 
-let users = [
-  { name: "Kishore", age: 24, course: "Bachelor of IT" },
-  { name: "Pranish", age: 25, course: "Cyber Security" },
-  { name: "Sirisha", age: 24, course: "Data Science" }
-];
-let list=document.getElementById("list");
-users.forEach(user=>{
-  let li=document.createElement("li");
-  li.innerText=`${user.name}-${user.age}-${user.course}`;
-  list.appendChild(li);
-});
+// let users = [
+//   { name: "Kishore", age: 24, course: "Bachelor of IT" },
+//   { name: "Pranish", age: 25, course: "Cyber Security" },
+//   { name: "Sirisha", age: 24, course: "Data Science" }
+// ];
+// let list=document.getElementById("list");
+// users.forEach(user=>{
+//   let li=document.createElement("li");
+//   li.innerText=`${user.name}-${user.age}-${user.course}`;
+//   list.appendChild(li);
+// });
 
-localStorage.setItem("users",JSON.stringify(users));
-let x=JSON.parse(localStorage.getItem("users"));
-console.log(x);
+// localStorage.setItem("users",JSON.stringify(users));
+// let x=JSON.parse(localStorage.getItem("users"));
+// console.log(x);
--- JS/json.js
+++ JS/json.js
@@ -1,17 +1,48 @@
-// let student={name:"kishore",age:24};
-// let x=JSON.stringify(student);
-// console.log(x);
-// let y=JSON.parse(x);
-// console.log(y);
+let users = [];
 
-// let promise=new Promise(function(resolve,reject){
-// let success=false;
-// if(success){
-//   resolve("Data Received");
-// }else{
-//   reject("It is not received")
-// }
-// });
-// promise
-// .then(res=>console.log(res))
-// .catch(err=>console.log(err));
+window.onload = function(){
+  let savedUsers = localStorage.getItem("users");
+
+  if (savedUsers){
+    users = JSON.parse(savedUsers);
+    displayUsers();
+  }
+};
+
+function addUser(){
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  let course = document.getElementById("course").value;
+
+  if(name === "" || age === "" || course === ""){
+    alert("Please fill all spaces");
+    return;
+  }
+
+  let user = {
+    name: name,
+    age: age,
+    course: course
+  };
+
+  users.push(user);
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  displayUsers();
+
+  document.getElementById("name").value = "";
+  document.getElementById("age").value = "";
+  document.getElementById("course").value = "";
+}
+
+function displayUsers(){
+  let list = document.getElementById("list");
+  list.innerHTML = "";
+
+  users.forEach(function(user){
+    let li = document.createElement("li");
+    li.innerText = `${user.name} - ${user.age} - ${user.course}`;
+    list.appendChild(li);
+  });
+}
